c83c33f98e9fd7ec6b5c679bb84912bf4c2e96fa
gabsrps <gabriel.santos@sou.inteli.edu.br>
2022-06-21 19:57:00 -0300

    Personalização etapa 7

---
+++ 03_AUT_EST_ENTREGA/Semana 8/backend/app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const hostname = '127.0.0.1';
+const port = 3026;
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+
+const DBPATH = 'dados.db';
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(express.static("../frontend/"));
+app.use(express.json())
+
+app.get('/user', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+ 	var sql = 'SELECT * FROM contatos';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/userinsert', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "INSERT INTO contatos (endereco, telefone, email) VALUES ('" + req.body.endereco + "', '" + req.body.telefone + "','" + req.body.email + "')";
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+    });
+    db.close(); // Fecha o banco
+    res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "UPDATE contatos SET nome = '" + req.body.nome + "' WHERE id = " + req.body.id;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "DELETE FROM contatos WHERE id = " + req.body.id;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
